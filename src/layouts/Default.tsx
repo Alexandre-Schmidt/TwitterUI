fa5a1b724329d2b59a6b6d4cef5b58bc0e500fb5
Alexandre I. P. Borges Schmidt <ale.tec.hma@gmail.com>
2023-04-03 20:36:54 -0300

    removing comments

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 
 import { Sidebar } from "../components/Sidebar";
-// agora tanto a Sidebar e o Outlet( Os conteúdos ) estão dentro do contexto das rotas
+
 
 import "./Default.css";
 
